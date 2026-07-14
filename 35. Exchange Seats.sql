9349c201cc48fccc2b24449446c0811a96cf5fa4
Likitha <likithasuguna2006@gmail.com>
2026-07-14 21:32:31 +0530

    Exchange Seats

---
+++ 35. Exchange Seats.sql
@@ -0,0 +1,11 @@
+-- Leetcode 626
+
+SELECT
+    CASE
+        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
