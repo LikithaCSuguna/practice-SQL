df9da12956ced5b908e24d96042d2358408fbd31
Likitha <likithasuguna2006@gmail.com>
2026-07-22 21:36:54 +0530

    Bank Account Summary 2

---
+++ 63. Bank Account Summary 2.sql
@@ -0,0 +1,10 @@
+-- Leetcode 1587
+
+SELECT
+    u.name,
+    SUM(t.amount) AS balance
+FROM Users u
+JOIN Transactions t
+    ON u.account = t.account
+GROUP BY u.account, u.name
+HAVING SUM(t.amount) > 10000;
